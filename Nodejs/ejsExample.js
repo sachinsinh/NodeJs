e8d6f583671c5cb79631e10b99ee2e7c68f06767
KILLERebel <94536748+sachinsinh@users.noreply.github.com>
2023-06-30 12:31:29 -0400

    ejsExample.js

--- Nodejs/ejsExample.js
+++ Nodejs/ejsExample.js
@@ -6,12 +6,17 @@ const expr = express();
 
 expr.set("view engine", "ejs");
 
+expr.get("/login", (req, resp) => {
+  resp.render("login");
+});
+
 expr.get("/profile", (req, resp) => {
   const user = {
     name: "sachinsinh",
     gmail: "sachn123@gmail.com",
     number: 98464646845,
+    skil: ["php", "javascript", "nodejs", "ejs", "c", "c++"],
   };
   resp.render("profile", { user });
 });
-expr.listen(4000);
+expr.listen(1000);
